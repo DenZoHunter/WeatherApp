bf410b3f2bb4e3101f5337c500c826e1b66cb298
Denis <denzohunter@bk.ru>
2021-05-30 18:24:04 +0300

    lesson 3, details weather, list cities

--- app/src/main/java/ru/geekbrains/weatherapp/model/City.kt
+++ app/src/main/java/ru/geekbrains/weatherapp/model/City.kt
@@ -1,7 +1,11 @@
 package ru.geekbrains.weatherapp.model
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class City(
     val city: String,
-    val lat: Double,
-    val lon: Double
-)
+    val lat: Double, //ширита
+    val lon: Double //долгота
+) : Parcelable
--- app/src/main/java/ru/geekbrains/weatherapp/viewmodel/MainViewModel.kt
+++ app/src/main/java/ru/geekbrains/weatherapp/viewmodel/MainViewModel.kt
@@ -2,7 +2,6 @@ package ru.geekbrains.weatherapp.viewmodel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import ru.geekbrains.weatherapp.viewmodel.AppState
 import ru.geekbrains.weatherapp.model.Repository
 import ru.geekbrains.weatherapp.model.RepositoryImpl
 import java.lang.Thread.sleep
@@ -15,14 +14,19 @@ class MainViewModel(
 
     fun getLiveData() = liveDataToObserve
 
-    fun getWeatherFromLocalSource() = getDataFromLocalSource()
-    fun getWeatherFromRemoteSource() = getDataFromLocalSource()
+    fun getWeatherFromLocalSourceRus() = getDataFromLocalSource(isRussian = true)
 
-    private fun getDataFromLocalSource() {
+    fun getWeatherFromLocalSourceWorld() = getDataFromLocalSource(isRussian = false)
+
+    fun getWeatherFromRemoteSource() = getDataFromLocalSource(isRussian = true)
+
+    private fun getDataFromLocalSource(isRussian: Boolean) {
         liveDataToObserve.value = AppState.Loading
         Thread {
             sleep(1000)
-            liveDataToObserve.postValue(AppState.Success(repositoryImpl.getWeatherFromLocalStorage()))
+            liveDataToObserve.postValue(AppState.Success(if (isRussian) repositoryImpl.getWeatherFromLocalStorageRus()
+            else repositoryImpl.getWeatherFromLocalStorageWorld()))
         }.start()
     }
 }
+
