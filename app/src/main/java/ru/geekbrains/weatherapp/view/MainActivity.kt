1fd00915891acc0403e99d1dd3400ebdad4a9fd3
Denis <denzohunter@bk.ru>
2021-06-08 21:53:41 +0300

    lesson 5, yandex API

--- app/src/main/java/ru/geekbrains/weatherapp/view/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/weatherapp/view/MainActivity.kt
@@ -3,17 +3,22 @@ package ru.geekbrains.weatherapp.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import ru.geekbrains.weatherapp.R
+import ru.geekbrains.weatherapp.databinding.MainActivityBinding
 import ru.geekbrains.weatherapp.view.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: MainActivityBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
+        binding = MainActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //binding.ok.setOnClickListener(clickListener)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance())
-                .commitNow()
+                .commitAllowingStateLoss()
         }
     }
 }
